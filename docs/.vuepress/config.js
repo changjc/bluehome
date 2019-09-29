2e8d9f85eb991a533c2cc42d2d56214d523c0dcd
changjc <cjc_mail@126.com>
2019-09-29 18:32:57 +0800

    add dest

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,18 @@
 module.exports = {
   title: 'BlueHOME',
   description: '网站描述',
+  dest:'./dist',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/bluehome/', // 这是部署到github相关的配置 下面会讲
+  base: './', // 这是部署到github相关的配置 下面会讲
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
   themeConfig: {
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-    lastUpdated: 'Last Updated' // 文档更新时间：每个文件git最后提交的时间
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     nav:[
       { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
       { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
@@ -26,7 +27,7 @@ module.exports = {
           }
         ]
       }        
-    ]
+    ],
     sidebar:{
         // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
         '/accumulate/': [
